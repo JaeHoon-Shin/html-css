ce7bbe7bdf6b849e993f7e9a2d9750aff5399936
JaeHoon-Shin <sinhoon0805@naver.com>
2022-12-12 18:20:10 +0900

    load

--- javascript/ajax/index.js
+++ javascript/ajax/index.js
@@ -12,7 +12,7 @@ let idx;
 function init(data) {
     const elView = document.querySelector('.mainView div')
     elThumb = document.querySelector('.thumb'),
-        mainV = document.querySelector('.mainView');
+    elP = document.querySelector('.view p');
     let thumbImg = "";
     let pos = { w: 0, h: 0, x: 0, y: 0 };
     data.forEach(function (el, key) {
@@ -29,31 +29,20 @@ function init(data) {
     pos.h = elView.offsetHeight;
     pos.x = elView.offsetLeft;
     pos.y = elView.offsetTop;
-    let idxPoint = "";
     elImg = document.querySelectorAll('.thumb figure');
 
     elImg.forEach(function (el, key) {
 
         el.onclick = function () {
-            /* console.log(elView.offsetWidth,elView.offsetHeight,elView.offsetTop , elView.offsetLeft )  */
-            idxPoint = (el.offsetWidth, el.offsetHeight, el.offsetLeft, el.offsetTop)
-            /*  el.style =`width:${pos.w}px; height : ${pos.h}px; left : ${pos.x}px; top : ${pos.y}px; position: absolute; transition: 500ms; `
-             if(idx!=null)
-             elImg[idx].style = `width:${idxPoint[0]}px; height : ${idxPoint[1]}px; left : ${idxPoint[2]}px; top : ${idxPoint[3]}px; position: absolute; transition: 500ms; `
-             idx = key;*/
-
-            /* elView.classList.add('on') */
             elView.innerHTML = `
-                <img class="newphoto" style="width:${el.offsetWidth}px; height : ${el.offsetHeight}px; left : ${el.offsetLeft}px; top : ${el.offsetTop}px; position: absolute; "; src="${data[key].url}">
-                <p>${data[key].detail}</p>
+            <img class="newphoto" style="width:${el.offsetWidth}px; height : ${el.offsetHeight}px; left : ${el.offsetLeft}px; top : ${el.offsetTop}px; position: absolute; "; src="${data[key].url}">
+               
             `
-            const imgMove = document.querySelector('.mainView div img');
+            elP.innerText = `${data[key].detail}` 
+            /* const imgMove = document.querySelector('.mainView div img'); */
 
-            /* imgMove.style=`width:${ pos.w}px; height : ${pos.h}px; left :${pos.x}; top : ${pos.y}px;` */
-            /* imgMove.style =  */
-            /* imgMove.animate({},2000); */
-            $('.mainView div img').animate({ `"width:${${ pos.w}}px; height : ${pos.h}px; left :${pos.x}; top : ${pos.y}px;"`},1000)
-        }
+            $('.mainView div img').animate({ width:`${ pos.w }px`, height: `${ pos.h}px`, left:`${ pos.x }px`, top: `${ pos.y }px` },1000)
+    }
     })
 
 
--- javascript/ajax/load/index.js
+++ javascript/ajax/load/index.js
@@ -1,5 +1,20 @@
 $('body').prepend('<header>');
 $('body').append('<footer>');
 
-$('header').load('./inc.html header' );
+$('header').load('./inc.html header',head );
 $('footer').load('./inc.html footer' );
+let idx = localStorage.idx || 0;
+function head(){
+    $('header a').eq(idx).css('color','red');
+    $('header a').click(function(){
+       let idx = $(this).index();
+       localStorage.idx = idx;  
+    /* 
+        localStorage.setItem('idx',2); idx값 수정
+        localStorage.getItem('idx');    가져오기
+        localStorage.removeItem();      삭제
+        localStorage.clear();           로컬스토리지 모두 삭제
+    */
+    })
+}
+
