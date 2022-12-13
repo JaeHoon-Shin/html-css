1ff28296b4c30dd8abdc894552ad4ab200de2600
JaeHoon-Shin <sinhoon0805@naver.com>
2022-12-13 18:24:26 +0900

    12.13수업 페이지 스크롤

--- javascript/load-page/index.js
+++ javascript/load-page/index.js
@@ -9,18 +9,14 @@ let css = `
             .active{opacity:1;}
         </style>`;
 $('body').append(css);
-
-
 function pageLoad(pageUrl,i) {
     $('main').removeClass('active');
     setTimeout(function () {
         $('main').load(pageUrl, function () {
             $(this).addClass('active');
-            /* addClass(),removeClass(),toggleClass(),hasClass() = addClass.conteins  */
-            
+            /* addClass(),removeClass(),toggleClass(),hasClass() = addClass.conteins  */  
         });
     },500)
-    
     $('header a').css('color', 'blue').eq(i).css('color', 'red');
 }
 
@@ -35,18 +31,18 @@ $('header a').on('click', function (e) {
 });
 $('header a:first').trigger('click');
 $(window).on('popstate',function(){
-    if(history.state.page!=null){
+    console.log('aa')
     let idx = history.state.page;
     let url = history.state.url;
-    pageLoad(url, idx);}
-})
-/* 
-[비동기 방식]
-Ajax( load, fetch. $.ajax ) 
-웹서버에 페이지를 요청(request) -> 웹서버에서 페이지를 받음(response)
+    pageLoad(url, idx);
+};
 
-click, setTimeout, setInterval
 
-[동기]
-브라우저가 문서의 순서에 따라서 실행하는 방식
- */
+// [비동기 방식]
+// Ajax( load, fetch. $.ajax ) 
+// 웹서버에 페이지를 요청(request) -> 웹서버에서 페이지를 받음(response)
+
+// click, setTimeout, setInterval
+
+// [동기]
+// 브라우저가 문서의 순서에 따라서 실행하는 방식
